ebcec60691246dc724265a099d565fd3581106cb
Jim Remsik <jremsikjr@gmail.com>
2013-11-30 22:38:00 -0600

    Add support for adding projects

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,31 +1,118 @@
 angular.module('todo', ['ionic'])
+/**
+ * The Projects factory handles saving and loading projects
+ * from local storage, and also lets us save and load the
+ * last active project index.
+ */
+.factory('Projects', function() {
+  return {
+    all: function() {
+      var projectString = window.localStorage['projects'];
+      if(projectString) {
+        return angular.fromJson(projectString);
+      }
+      return [];
+    },
+    save: function(projects) {
+      window.localStorage['projects'] = angular.toJson(projects);
+    },
+    newProject: function(projectTitle) {
+      // Add a new project
+      return {
+        title: projectTitle,
+        tasks: []
+      };
+    },
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveProject']) || 0;
+    },
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveProject'] = index;
+    }
+  }
+})
 
-.controller('TodoCtrl', function($scope, Modal) {
-  // No need for testing data anymore
-  $scope.tasks = [];
+.controller('TodoCtrl', function($scope, $timeout, Modal, Projects) {
 
-  // Create and load the Modal
+  // A utility function for creating a new project
+  // with the given projectTitle
+  var createProject = function(projectTitle) {
+    var newProject = Projects.newProject(projectTitle);
+    $scope.projects.push(newProject);
+    Projects.save($scope.projects);
+    $scope.selectProject(newProject, $scope.projects.length-1);
+  }
+
+
+  // Load or initialize projects
+  $scope.projects = Projects.all();
+
+  // Grab the last active, or the first project
+  $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
+
+  // Called to create a new project
+  $scope.newProject = function() {
+    var projectTitle = prompt('Project name');
+    if(projectTitle) {
+      createProject(projectTitle);
+    }
+  };
+
+  // Called to select the given project
+  $scope.selectProject = function(project, index) {
+    $scope.activeProject = project;
+    Projects.setLastActiveIndex(index);
+    $scope.sideMenuController.close();
+  };
+
+  // Create our modal
   Modal.fromTemplateUrl('new-task.html', function(modal) {
     $scope.taskModal = modal;
   }, {
-    scope: $scope,
-    animation: 'slide-in-up'
+    scope: $scope
   });
 
-  // Called when the form is submitted
   $scope.createTask = function(task) {
-    $scope.tasks.push(task);
+    if(!$scope.activeProject) {
+      return;
+    }
+    $scope.activeProject.tasks.push({
+      title: task.title
+    });
     $scope.taskModal.hide();
+
+    // Inefficient, but save all the projects
+    Projects.save($scope.projects);
+
+    task.title = "";
   };
 
-  // Open our new task modal
   $scope.newTask = function() {
     $scope.taskModal.show();
   };
 
-  // Close the new task modal
   $scope.closeNewTask = function() {
     $scope.taskModal.hide();
   }
-});
 
+  $scope.toggleProjects = function() {
+    $scope.sideMenuController.toggleLeft();
+  };
+
+
+  // Try to create the first project, make sure to defer
+  // this by using $timeout so everything is initialized
+  // properly
+  $timeout(function() {
+    if($scope.projects.length == 0) {
+      while(true) {
+        var projectTitle = prompt('Your first project title:');
+        if(projectTitle) {
+          createProject(projectTitle);
+          break;
+        }
+      }
+    }
+  });
+
+});
