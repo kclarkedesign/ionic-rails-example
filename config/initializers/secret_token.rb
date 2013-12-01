61819101297caef2aae2310878f99252c2d17954
Jim Remsik <jremsikjr@gmail.com>
2013-11-30 22:11:04 -0600

    rails new ionic-rails-example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IonicRailsExample::Application.config.secret_key_base = 'd7f3a7df261a26abe416b2b5b21940ae1082bede5a500710f8bac4fb564abbed6677f9a3f986db79a63870b3ecab81e589cffdceaa24fdb3e8e148a28ae711f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IonicRailsExample::Application.config.session_store :cookie_store, key: '_ionic-rails-example_session'
