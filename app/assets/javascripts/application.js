dcc29ccfa59c0ec44ff884fd5bd55454d9bb6650
Jim Remsik <jremsikjr@gmail.com>
2013-11-30 22:14:26 -0600

    Add ionic resources to manifest files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,17 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require angular/angular
+//= require angular/angular-animate
+//= require angular/angular-cookies
+//= require angular/angular-loader
+//= require angular/angular-mocks
+//= require angular/angular-resource
+//= require angular/angular-route
+//= require angular/angular-sanitize
+//= require angular/angular-scenario
+//= require angular/angular-touch
+//= require ionic
+//= require ionic-angular
 //= require turbolinks
 //= require_tree .
